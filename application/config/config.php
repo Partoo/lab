5044c6400de901442314058a16b442382cc06460
Partoo <Partoo@163.com>
2013-01-24 19:48:50 +0800

    modify personal config

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'rPN98Xg0srASv57Cw8QBD5VSMXXW6tt4';
 
 /*
 |--------------------------------------------------------------------------
